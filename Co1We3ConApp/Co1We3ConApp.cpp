c6fcc5e32fd4bfc3bdacf6eb535a7cacc578d5c2
Kaydyn Wilson <maeraltrinity@gmail.com>
2025-02-06 10:03:42 -0600

    Weird Saved Function in previous
    The save function was blue instead of green so I just completely restarted the file.

---
+++ Co1We3ConApp/Co1We3ConApp.cpp
@@ -0,0 +1,14 @@
+// Kaydyn Wilson SP2025 Coding 1 Week 3 
+// Variables, Logic, and Randomness
+
+#include <iostream>		// in out stream sends text to and from the console. 
+#include <string>		// allows us to use sring variables.
+using namespace std;	// so we don't have to type std : : all the time. 
+
+// this is where the program starts running!
+// it's a function that returns an integer. 
+int main() {
+	// all code inside a code block should be indented 
+	cout << "\tWelcome to Week 3!\n";
+	cout << "We're talking about Variables, Logic, and Randomness today.\n";
+}	// end of main ()
--- c1w3ca/c1w3ca.cpp
+++ c1w3ca/c1w3ca.cpp
@@ -1,20 +1,5 @@
-// c1w3ca.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// Kaydyn Wilson Coding 1 SP2025 Week 3
+// Variables, Logic, and Randomness
 
-#include <iostream>
-
-int main()
-{
-    std::cout << "Hello World!\n";
-}
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+#include <iostream>		// sends text to and from the console.
+#include <string>		// allows us to use sttring variables. 
