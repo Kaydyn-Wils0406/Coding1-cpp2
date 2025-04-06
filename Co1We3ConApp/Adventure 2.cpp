d3466ad688272bfc71a282f1d42bbc1de7edff33
Kaydyn Wilson <maeraltrinity@gmail.com>
2025-04-06 12:14:53 -0500

    Additions to HW
    I was able to get help in class to better understand what I was doing.

--- Co1We3ConApp/Adventure 2.cpp
+++ Co1We3ConApp/Adventure 2.cpp
@@ -5,60 +5,119 @@
 
 using namespace std;
 
+int playerHealth = 25;
+int totalTreasure = 0;
+int enemyHealth = 10;
+
 bool debug = true;
 
 void startStory() {
-	cout << "You explore a forest and come across a beast.\n";		// This is the only thing I am confident in. 
-}
+	string input;
+	int playerHealth = 25;
+	int totalTreasure = 0;
+	int enemyHealth = 10;
 
-
-bool askYesNo(int input) {
-	// I could not figure this out no matter how many different ways I tried or videos I looked up. I don't know how to make it take an input that isnt a number.
-	//Is this even supposed to be a bool?
-}
-bool adventure() {
-	int number = rand() % 10;
-
-	if (number > 4) {
-		return true;			// I am supposed to use this with the actualAdventure but I don't know how to connect them. 
+	cout << "Would you like to go adventuring? Type 'Yes' to play and 'No' to end the program.\n";
+	
+	if (input == "Yes") {	
+		cout << "With " << playerHealth << " total health, you head into the forest and come across a beast!\n";
+		askYesNo();
 	}
 	else {
-		return false;
+		ending();
 	}
 }
 
-string actualAdventure(int attack, int block) {
-	string input;
-	int attack = (rand() % 5);
-	int block = (rand() % 5); 
 
+bool askYesNo() {
+	string input;														// I could not figure this out no matter how many different ways I tried or videos I looked up. I don't know how to make it take an input that isnt a number.
+																//Is this even supposed to be a bool?
 	do {
-		cout << "Would you like to attack the beast?\n"; 
+		cout << "Would you like to attack the beast?\n";
 		getline(cin, input);
 
 		if (input == "Yes") {
-			cout << "You rolled a \n";				// I need to be able to do the yes and no for this one to work because if I try to 
-													// base if off of the input, I won't be able to do an if then about the numbers. 
+			cout << "You attacked the beast!\n";				// I need to be able to do the yes and no for this one to work because if I try to 
+			actualAdventure();										// base if off of the input, I won't be able to do an if then about the numbers. 
 		}
+		else if (input == "No") {
+			ending();
+			break;
 
+		}
+	} while (true);
+}
+
+void actualAdventure() {
+	while (true) {
+		int attack = rollDie();
+		int block = rollDie();
+		if (attack > block) {
+			cout << "You have successfully attacked the beast for " << attack << " damage!\n";
+			enemyHealth -= attack;
+		}
+		else if (block >= attack) {
+			cout << "The beast has blocked your attack and attacked you back for " << attack << " damage.\n";
+			playerHealth -= attack;
+		}
+		if (playerHealth < 1) {
+			death();
+			
+		}
+		else if (enemyHealth < 1) {
+			cout << "You have successfully defeated the beast!\n";
+			totalTreasure += rollTreasure();
+			cout << "You now have " << totalTreasure << " gold coins!\n";
+			retry();
+		}
+	}
+}
+int rollDie(int sides = 6) {
+	return (rand() % sides);
+}
+
+int rollTreasure(int faces = 20) {
+	return (rand() % faces);
+}
+
+void ending() {
+	cout << "You head home to saftey with a total of " << totalTreasure << " gold coins.\n";
+	if (playerHealth < 2) {
+		cout << "You should seek medical attention with the nearest doctor. Your total health is " << playerHealth << ".\n";
+		retry();
+	}
+}
+void death() {
+	cout << "Tou have taken too much damage and the beast has defeated you! You have lost all your treasure and your life!\n";
+	retry();
+}
+bool retry(string question = "Would you like to try again?\n") {
+	do {
+		string input;
+		cout << question;
+		getline(cin, input);
+
+		if (input == "Yes") {
+			startStory();
+		
+		}
+		else if (input == "No") {
+			break;
+		}
+		else {
+			cout << "Please choose between 'Yes' and 'No' to try again or end program.\n";
+		}
 	} while (true);
 } 
-int rollOrDie(int sides = 6) {
-
-}
 
 int main() {
 
 	srand(time(0));
 
-	int health = 10;
-	int totalTreasure = 0;		// got this from the previous adventure assignment
+	
 
 	startStory(); 
 
-	if (adventure()) {
-		cout << "You've won a prize!!\n";
-		// if true, call adventure
-	}
+	
 }
 
